34eb3a8de871e0daacb9d34028b152f73ffded86
Neelam0309 <neelamkpatil@gmail.com>
2018-03-23 14:45:52 +0800

    Added Dao test files for userhelper and traildao

---
+++ app/src/test/java/sg/edu/iss/trailblazelearnft04/DBDao/UserHelperDaoTest.java
@@ -0,0 +1,71 @@
+package sg.edu.iss.trailblazelearnft04.DBDao;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+
+import org.mockito.stubbing.Answer;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import sg.edu.iss.trailblazelearnft04.Activity.MainActivity;
+import sg.edu.iss.trailblazelearnft04.Model.User;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Neelam on 3/22/2018.
+ */
+@RunWith(PowerMockRunner.class)
+public class UserHelperDaoTest {
+
+
+
+    FirebaseAuth mockedAuth;
+    private DatabaseReference mockedDatabaseReference;
+    @Mock
+    DataSnapshot dataSnapshot;
+    @Mock
+    ValueEventListener valueEventListener;
+    @Mock
+    User expectedUser;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
+
+        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
+      //  when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
+       when(dataSnapshot.getValue(User.class)).thenReturn(expectedUser);
+
+    }
+
+    @Test
+    public void setUserNameByUserId() throws Exception {
+
+        User resutlUser;
+        valueEventListener.onDataChange(dataSnapshot);
+        {
+            resutlUser = dataSnapshot.getValue(User.class);
+        }
+        assertNotNull(resutlUser);
+        assertThat(resutlUser, is(expectedUser));
+    }
+
+
+}
